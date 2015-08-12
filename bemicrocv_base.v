85714b96128272a7b21fcc922762318bc4df798e
7akase <yasuhide.takase@gmail.com>
2015-08-12 18:39:33 +0900

    add BeMicro pin info to sqf file

--- bemicrocv_base.v
+++ bemicrocv_base.v
@@ -1,17 +1,19 @@
-module bemicrocv_base(clk_i, led0);
+module bemicrocv_base(clk_24mhz, user_led0);
 
-input clk_i;
-output led0;
+input clk_24mhz;
+output user_led0;
 
 parameter W_CNT = 8;
 
-reg             led0;
+reg             user_led0;
 reg [W_CNT-1:0] cnt;
+
+assign clk_i = clk_24mhz;
 
 always @(posedge clk_i)
    if(~|cnt) begin
 	  cnt <= {W_CNT{1'b1}};
-	  led0 <= ~led0;
+	  user_led0 <= ~user_led0;
    end
 	
 endmodule
