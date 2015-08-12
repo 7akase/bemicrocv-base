5a87750e9f73b845fee4957a937aa2b7f360286b
7akase <yasuhide.takase@gmail.com>
2015-08-13 01:44:46 +0900

    fix pin assignment

--- bemicrocv_base.v
+++ bemicrocv_base.v
@@ -1,20 +1,26 @@
-module bemicrocv_base(clk_24mhz, user_led0);
+module bemicrocv_base(CLK_24MHz, Tact1, USER_LED1);
 
-input clk_24mhz;
-output user_led0;
+input CLK_24MHz;
+input Tact1;
+output USER_LED1;
 
-parameter W_CNT = 8;
+parameter TACT_ON  = 1'b0;
+parameter TACT_OFF = 1'b1;
 
-reg             user_led0;
+parameter W_CNT = 23;
+
+reg             USER_LED1;
 reg [W_CNT-1:0] cnt;
 
-assign clk_i = clk_24mhz;
-
-always @(posedge clk_i)
-   if(~|cnt) begin
+always @(posedge CLK_24MHz) begin
+   if(Tact1 == TACT_ON) begin
 	  cnt <= {W_CNT{1'b1}};
-	  user_led0 <= ~user_led0;
-   end
-	
+	end else if(~|cnt) begin
+	  cnt <= {W_CNT{1'b1}};
+	  USER_LED1 <= ~USER_LED1;
+	end else begin
+	  cnt <= cnt - {{(W_CNT-1){1'b0}}, 1'b1};
+	end
+end
 endmodule
 
